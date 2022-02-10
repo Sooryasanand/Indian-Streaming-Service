1fb2ffea750c13dc962f89fe94d4f6a466e685ab
Soorya Narayanan Sanand <sooryasanand@Sooryas-MacBook-Pro.local>
2022-02-10 11:53:49 +1100

    Changed Color

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Nav.css';
+import logo from './Image/favicon.ico';
 
 function Nav() {
 const [show, handleshow] = useState(false);
@@ -23,7 +24,7 @@ useEffect(() => {
     return (
         <div className={`nav ${show && 'nav_bg'}`}>
             <div className='nav_contents'>
-                <h1 className='nav_logo' onClick={() => navigate('/')}>Stream Flex</h1>
+                <img className='nav_logo' onClick={() => navigate('/')} src={logo} alt="logo" />
                 <img onClick={() => navigate('/profile')} className='nav_avatar' src="https://streamflexmisc.s3.ap-south-1.amazonaws.com/profilePic/profilePicBlue.png" />
             </div>
         </div>
